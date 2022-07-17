9441e1eafc361e7bcf1e5d8e7eaa6f75a2046bb7
Nike2406 <locus2@mail.ru>
2022-07-17 21:00:07 +0300

    added raw vector

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include "./srcs/vector.hpp"
+#include <vector>
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    ft::
     return 0;
 }
---
+++ srcs/vector.hpp
@@ -0,0 +1,23 @@
+#ifndef FT_CONTAINERS_VECTOR_HPP
+#define FT_CONTAINERS_VECTOR_HPP
+
+namespace ft {
+    template < class T, class Allocator = std::allocator<T> >
+            class vector {
+            public:
+                typedef T value_type;
+                typedef Allocator allocator_type;
+                typedef typename allocator_type::reference reference;
+                typedef typename allocator_type::const_reference const_reference;
+                typedef typename allocator_type::pointer pointer;
+                typedef typename allocator_type::const_pointer const_pointer;
+                typedef T iterator;
+                typedef T const_iterator;
+                typedef T reverse_iterator;
+                typedef T const_reverse_iterator;
+                typedef T difference_type;
+                typedef T size_type;
+            };
+}
+
+#endif
