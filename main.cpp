2059379bf61e92f4255985f2e9f3d62e01473824
Nike2406 <locus2@mail.ru>
2022-07-20 20:06:21 +0300

    start to implement vector

--- main.cpp
+++ main.cpp
@@ -21,6 +21,22 @@ int main() {
     std::cout << "Is empty? " << myStack.empty() << std::endl;
     std::cout << "================================" << std::endl;
 
+    std::cout << "Vector" << std::endl;
+    ft::stack< int> myStack;
+    myStack.push(1);
+    myStack.push(2);
+    myStack.push(3);
+    myStack.push(4);
+    myStack.pop();
+    myStack.empty();
+    int size = myStack.size();
+    for (int i = 0; i < size; i++) {
+        std::cout << myStack.top() << std::endl;
+        myStack.pop();
+    }
+    std::cout << "Is empty? " << myStack.empty() << std::endl;
+    std::cout << "================================" << std::endl;
+
 
     return 0;
 }
