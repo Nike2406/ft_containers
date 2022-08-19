8a19d6bd238946fe73e31bbac3ed43c7f901481c
Nike2406 <locus2@mail.ru>
2022-08-19 10:33:30 +0300

    added vector fill constructor

--- main.cpp
+++ main.cpp
@@ -1,42 +1,67 @@
 #include <iostream>
-#include "./srcs/vector.hpp"
-#include "./srcs/stack.hpp"
+//#include "./srcs/vector.hpp"
+//#include "./srcs/stack.hpp"
 #include <vector>
 #include <stack>
 
+void printContainer(std::string containerType, std::vector<int> myVector);
+
 int main() {
-    std::cout << "Stack" << std::endl;
-    ft::stack< int> myStack;
-    myStack.push(1);
-    myStack.push(2);
-    myStack.push(3);
-    myStack.push(4);
-    myStack.pop();
-    myStack.empty();
-    int size = myStack.size();
-    for (int i = 0; i < size; i++) {
-        std::cout << myStack.top() << std::endl;
-        myStack.pop();
-    }
-    std::cout << "Is empty? " << myStack.empty() << std::endl;
-    std::cout << "================================" << std::endl;
+//    std::cout << "Stack" << std::endl;
+//    ft::stack< int> myStack;
+//    myStack.push(1);
+//    myStack.push(2);
+//    myStack.push(3);
+//    myStack.push(4);
+//    myStack.pop();
+//    myStack.empty();
+//    int size = myStack.size();
+//    for (int i = 0; i < size; i++) {
+//        std::cout << myStack.top() << std::endl;
+//        myStack.pop();
+//    }
+//    std::cout << "Is empty? " << myStack.empty() << std::endl;
+//    std::cout << "================================" << std::endl;
+//
+//    std::cout << "Vector" << std::endl;
+//    std::vector<int> myVector;
+//    myStack.push(1);
+//    myStack.push(2);
+//    myStack.push(3);
+//    myStack.push(4);
+//    myStack.pop();
+//    myStack.empty();
+//    size = myStack.size();
+//    for (int i = 0; i < size; i++) {
+//        std::cout << myStack.top() << std::endl;
+//        myStack.pop();
+//    }
+//    std::cout << "Is empty? " << myStack.empty() << std::endl;
+//    std::cout << "================================" << std::endl;
 
-    std::cout << "Vector" << std::endl;
+    /**** Check ****/
+    std::string containerType = "Vector";
     std::vector<int> myVector;
-    myStack.push(1);
-    myStack.push(2);
-    myStack.push(3);
-    myStack.push(4);
-    myStack.pop();
-    myStack.empty();
-    size = myStack.size();
-    for (int i = 0; i < size; i++) {
-        std::cout << myStack.top() << std::endl;
-        myStack.pop();
-    }
-    std::cout << "Is empty? " << myStack.empty() << std::endl;
-    std::cout << "================================" << std::endl;
 
+    printContainer(containerType, myVector);
+    myVector.push_back(1);
+    myVector.push_back(2);
+    myVector.push_back(3);
+    myVector.push_back(4);
+    myVector.resize(545646546468464648646848648484848484864);
+    myVector.reserve(20);
+    printContainer(containerType, myVector);
 
     return 0;
+}
+
+void printContainer(std::string containerType, std::vector<int> myVector) {
+    std::cout << containerType << " max_size: " << myVector.max_size() << std::endl;
+    std::cout << containerType << " size: " << myVector.size() << std::endl;
+    std::cout << containerType << " capacity: " << myVector.capacity() << std::endl;
+    for (int i = 0; i < myVector.size(); ++i) {
+        std::cout << myVector[i] << " ";
+    }
+    std::cout << std::endl;
+
 }
--- srcs/vector.hpp
+++ srcs/vector.hpp
@@ -2,19 +2,20 @@
 #define FT_CONTAINERS_VECTOR_HPP
 
 #include <memory> // for std::allocator
+#include <iostream>
 
 namespace ft {
     template<class T, class Allocator = std::allocator<T> >
     class vector {
     public:
-        typedef T                                           value_type;
-        typedef Allocator                                   allocator_type;
-        typedef typename allocator_type::reference          reference;
-        typedef typename allocator_type::const_reference    const_reference;
-        typedef typename allocator_type::pointer            pointer;
-        typedef typename allocator_type::const_pointer      const_pointer;
-        typedef typename allocator_type::difference_type    difference_type;
-        typedef typename allocator_type::size_type          size_type;
+        typedef T value_type;
+        typedef Allocator allocator_type;
+        typedef typename allocator_type::reference reference;
+        typedef typename allocator_type::const_reference const_reference;
+        typedef typename allocator_type::pointer pointer;
+        typedef typename allocator_type::const_pointer const_pointer;
+        typedef typename allocator_type::difference_type difference_type;
+        typedef typename allocator_type::size_type size_type;
 
         typedef T iterator;
         typedef T const_iterator;
@@ -22,14 +23,13 @@ namespace ft {
         typedef T const_reverse_iterator;
 
     private:
-        pointer         _array;
-        size_type       _size;
-        size_type       _capacity;
-        allocator_type  _alloc;
+        pointer _array;
+        size_type _size;
+        size_type _capacity;
+        allocator_type _alloc;
 
     public:
         /***** Member functions ******/
-        /***** Canonical form ******/
         // default
         explicit vector(const allocator_type &alloc = allocator_type())
                 : _array(0), _size(0), _capacity(0), _alloc(alloc) {};
@@ -50,12 +50,44 @@ namespace ft {
         // range
         template<class InputIterator>
         vector(InputIterator first, InputIterator last,
-               const allocator_type &alloc = allocator_type()) {};
+               const allocator_type &alloc = allocator_type())
+                : _array(0), _size(0), _capacity(0), _alloc(alloc) {
+
+        };
 
         // copy
         vector(const vector &x) {};
 //////////////////////////////////
 
+        /***** Capacity ******/
+        // return vector size
+        size_type size() const      { return _size; }
+        // return maximum size which vector can allocate
+        size_type max_size() const  { return _alloc.max_size(); }
+        // change container size with n elements
+        void resize (size_type n, value_type val = value_type()) {
+            if (n > this->max_size())
+                throw std::length_error("vector::_M_fill_insert");
+            if (n == _size)
+                return;
+            else if (n < _size) {
+                while (_size > n) {
+                    // TODO realise after movement pop/push
+                    // _alloc.destroy(last)
+                }
+            }
+        }
+
+        // reserve n elements for container
+        void reserve(size_t n) { // std::vector::reserve
+            try {
+                if (_capacity < n) {
+                    pointer newArray = _alloc.allocate(n);
+                }
+            } catch (const std::length_error &le) {
+                std::cerr << "Length error: " << le.what() << '\n';
+            }
+        }
     };
 }
 
