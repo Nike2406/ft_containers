50ffa8a9d44e3005a379e0f53531de63cfc8ad65
Nike2406 <locus2@mail.ru>
2022-09-26 13:54:12 +0300

    added map

--- srcs/iterator_utils/node_iterator.hpp
+++ srcs/iterator_utils/node_iterator.hpp
@@ -4,11 +4,9 @@
 # include "iterator_traits.hpp"
 # include <stddef.h>
 
-namespace ft
-{
-    template <class T, class Pair>
-    class node_iterator
-    {
+namespace ft {
+    template<class T, class Pair>
+    class node_iterator {
     public:
         T node;
 
@@ -16,121 +14,112 @@ namespace ft
         typedef T                                               iterator_type;
         typedef Pair                                            value_type;
         typedef typename iterator_traits<T>::difference_type    difference_type;
-        typedef Pair&                                           reference;
-        typedef const Pair&                                     const_ref;
-        typedef Pair*                                           pointer;
-        typedef const Pair*                                     const_pointer;
+        typedef Pair                                            &reference;
+        typedef const Pair                                      &const_ref;
+        typedef Pair                                            *pointer;
+        typedef const Pair                                      *const_pointer;
         typedef typename iterator_traits<T>::iterator_category  iterator_category;
 
         /***** Member functions ******/
         node_iterator(T value = NULL) : node(value) {};
+
         ~node_iterator() {};
 
-        template <typename N1, typename N2>
+        template<typename N1, typename N2>
         node_iterator(const node_iterator<N1, N2> &other)
                 : node(other.node) {};
 
-        iterator_type base() const
-        {
+        iterator_type base() const {
             return node;
         }
-        node_iterator &operator=(const node_iterator &other)
-        {
+
+        node_iterator &operator=(const node_iterator &other) {
             if (this != &other)
                 node = other.node;
             return *this;
         }
-        node_iterator operator++(int)
-        {
+
+        node_iterator operator++(int) {
             node_iterator tmp = *this;
             increment();
             return tmp;
         }
-        node_iterator &operator++()
-        {
+
+        node_iterator &operator++() {
             increment();
             return *this;
         }
-        node_iterator operator--(int)
-        {
+
+        node_iterator operator--(int) {
             node_iterator tmp = *this;
             decrement();
             return tmp;
         }
-        node_iterator &operator--()
-        {
+
+        node_iterator &operator--() {
             decrement();
             return *this;
         }
-        reference operator*()
-        {
+
+        reference operator*() {
             return *(node->pair);
         }
-        const_ref operator*() const
-        {
+
+        const_ref operator*() const {
             return *(node->pair);
         }
-        pointer operator->()
-        {
+
+        pointer operator->() {
             return (node->pair);
         }
-        const_pointer operator->() const
-        {
+
+        const_pointer operator->() const {
             return (node->pair);
         }
-        bool operator==(node_iterator const &other) const
-        {
+
+        bool operator==(node_iterator const &other) const {
             return node == other.node;
         };
-        bool operator!=(node_iterator const &other) const
-        {
+
+        bool operator!=(node_iterator const &other) const {
             return node != other.node;
         };
-        bool operator>(node_iterator const &other) const
-        {
+
+        bool operator>(node_iterator const &other) const {
             return node->pair > other.node->pair;
         };
-        bool operator<(node_iterator const &other) const
-        {
+
+        bool operator<(node_iterator const &other) const {
             return other.node->pair > node->pair;
         };
-        bool operator<=(node_iterator const &other) const
-        {
+
+        bool operator<=(node_iterator const &other) const {
             return node->pair <= other.node->pair;
         };
-        bool operator>=(node_iterator const &other) const
-        {
+
+        bool operator>=(node_iterator const &other) const {
             return node->pair >= other.node->pair;
         };
 
-        void increment()
-        {
-            if (node->NIL)
-            {
+        void increment() {
+            if (node->NIL) {
                 if (node->begin != 0)
                     node = node->begin;
                 return;
-            }
-            else if (!node->right->NIL)
-            {
+            } else if (!node->right->NIL) {
                 node = node->right;
                 while (!node->left->NIL)
                     node = node->left;
-            }
-            else
-            {
+            } else {
                 T current = node;
                 T tmp = node;
                 node = node->parent;
-                if (!node)
-                {
+                if (!node) {
                     node = current->right;
                     return;
                 }
-                while (node->left != tmp)
-                {
-                    if (!node->parent)
-                    {
+                while (node->left != tmp) {
+                    if (!node->parent) {
                         node = current->right;
                         break;
                     }
@@ -140,41 +129,31 @@ namespace ft
             }
         }
 
-        void decrement()
-        {
-            if (node->NIL)
-            {
+        void decrement() {
+            if (node->NIL) {
                 node = node->parent;
                 return;
-            }
-            else if (node->NIL)
+            } else if (node->NIL)
                 return;
-            if (!node->left->NIL)
-            {
+            if (!node->left->NIL) {
                 node = node->left;
                 while (!node->right->NIL)
                     node = node->right;
-            }
-            else
-            {
+            } else {
                 T tmp = node;
-                if (!node->parent && node->right->NIL && node->left->NIL)
-                {
+                if (!node->parent && node->right->NIL && node->left->NIL) {
                     node = tmp->left;
                     return;
                 }
-                if (node->first)
-                {
+                if (node->first) {
                     *node->left->begin = *node;
                     node = node->left;
                     return;
                 }
                 node = node->parent;
-                while (node->right != tmp)
-                {
+                while (node->right != tmp) {
                     tmp = node;
-                    if (!node->parent)
-                    {
+                    if (!node->parent) {
                         node = tmp->left;
                         break;
                     }
--- srcs/utils/pair.hpp
+++ srcs/utils/pair.hpp
@@ -1,11 +1,9 @@
 #ifndef FT_CONTAINERS_PAIR_HPP
 #define FT_CONTAINERS_PAIR_HPP
 
-namespace ft
-{
-    template <class T1, class T2>
-    struct pair
-    {
+namespace ft {
+    template<class T1, class T2>
+    struct pair {
         /*
          * This class couples together a pair of values, which may be of different types (T1 and T2).
          * The individual values can be accessed through its public members first and second.
@@ -13,18 +11,16 @@ namespace ft
         typedef T1 first_type;
         typedef T2 second_type;
 
-        first_type  first;
+        first_type first;
         second_type second;
 
         pair(const first_type &x = first_type(), const second_type &y = second_type()) : first(x), second(y) {}
 
-        template <class P1, class P2>
+        template<class P1, class P2>
         pair(const pair<P1, P2> &other) : first(other.first), second(other.second) {}
 
-        pair &operator=(const pair &other)
-        {
-            if (this != &other)
-            {
+        pair &operator=(const pair &other) {
+            if (this != &other) {
                 first = other.first;
                 second = other.second;
             }
@@ -33,45 +29,38 @@ namespace ft
     };
 
     // Construct pair object
-    template <class P1, class P2>
-    pair<P1, P2> make_pair(P1 x, P2 y)
-    {
+    template<class P1, class P2>
+    pair<P1, P2> make_pair(P1 x, P2 y) {
         return pair<P1, P2>(x, y);
     }
 
-    template <class P1, class P2>
-    bool operator==(const pair<P1, P2> &x, const pair<P1, P2> &y)
-    {
+    template<class P1, class P2>
+    bool operator==(const pair<P1, P2> &x, const pair<P1, P2> &y) {
         return (x.first == y.first && x.second == y.second);
     }
 
-    template <class P1, class P2>
-    bool operator!=(const pair<P1, P2> &x, const pair<P1, P2> &y)
-    {
+    template<class P1, class P2>
+    bool operator!=(const pair<P1, P2> &x, const pair<P1, P2> &y) {
         return (x.first != y.first || x.second != y.second);
     }
 
-    template <class P1, class P2>
-    bool operator<(const pair<P1, P2> &x, const pair<P1, P2> &y)
-    {
+    template<class P1, class P2>
+    bool operator<(const pair<P1, P2> &x, const pair<P1, P2> &y) {
         return (x.first < y.first || (!(x.first < y.first) && x.second < y.second));
     }
 
-    template <class P1, class P2>
-    bool operator<=(const pair<P1, P2> &x, const pair<P1, P2> &y)
-    {
+    template<class P1, class P2>
+    bool operator<=(const pair<P1, P2> &x, const pair<P1, P2> &y) {
         return !(y < x);
     }
 
-    template <class P1, class P2>
-    bool operator>(const pair<P1, P2> &x, const pair<P1, P2> &y)
-    {
+    template<class P1, class P2>
+    bool operator>(const pair<P1, P2> &x, const pair<P1, P2> &y) {
         return (y < x);
     }
 
-    template <class P1, class P2>
-    bool operator>=(const pair<P1, P2> &x, const pair<P1, P2> &y)
-    {
+    template<class P1, class P2>
+    bool operator>=(const pair<P1, P2> &x, const pair<P1, P2> &y) {
         return !(x < y);
     }
 }
